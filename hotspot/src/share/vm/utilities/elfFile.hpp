fa75a5cef7019383da86fafc8350bdc0fe085228
ArtDev <45949002+artdeell@users.noreply.github.com>
2021-01-24 10:06:23 +0300

    Update elfFile.hpp

--- hotspot/src/share/vm/utilities/elfFile.hpp
+++ hotspot/src/share/vm/utilities/elfFile.hpp
@@ -47,7 +47,7 @@ typedef Elf64_Phdr      Elf_Phdr;
 typedef Elf64_Sym       Elf_Sym;
 
 #if !defined(_ALLBSD_SOURCE) || defined(__APPLE__)
-#define ELF_ST_TYPE ELF64_ST_TYPE
+//#define ELF_ST_TYPE ELF64_ST_TYPE
 #endif
 
 #else
@@ -64,7 +64,7 @@ typedef Elf32_Phdr      Elf_Phdr;
 typedef Elf32_Sym       Elf_Sym;
 
 #if !defined(_ALLBSD_SOURCE) || defined(__APPLE__)
-#define ELF_ST_TYPE ELF32_ST_TYPE
+//#define ELF_ST_TYPE ELF32_ST_TYPE
 #endif
 #endif
 
